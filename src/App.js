3d75ecef5e65f559565c539fdf984694ecae53b1
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-18 18:48:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+import './App.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit, faPlus);
+function App() {
+  return (
+    <div className="App">
+
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
